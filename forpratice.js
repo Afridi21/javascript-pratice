d295ea458db41ea08e0103ea21a9a03309e74298
afridi21 <abdullahalafridi5@gmail.com>
2022-08-10 19:10:40 +0600

    added

---
+++ forpratice.js
@@ -0,0 +1,109 @@
+// function covert(feet){
+//     const law=feet*12;
+//     return law;
+// }
+// const calculation=covert(5.6);
+// console.log(calculation.toFixed(2));
+
+
+
+// function calculated(marks){
+//     if (marks>33) {
+//         console.log("Pass");
+//     }
+//     else{
+//         console.log("Fail");
+//     }
+// }
+// const calculation=calculated(70)
+
+// function calculatedGpa(marks) {
+//     if(marks>33-39){
+//         console.log("D");
+//     }
+//     else if(marks>40-49){
+//         console.log("C");
+//     }
+//     else if(marks>50-59){
+//         console.log("B");
+//     }
+//     else if(marks>60-69){
+//         console.log("A");
+//     }
+//     else if(marks>70-79){
+//         console.log("A+");
+//     }
+// }
+// const marks=45;
+// const CGPA=calculatedGpa(marks)
+// console.log(CGPA);
+// console.log("You Got=",marks);
+
+// function iseven(number){ if(number%2===0){
+//         console.log("Yes!This is even number");
+//     }
+//     else{
+//         console.log("Opps!This is odd number");
+//     }
+// }
+// const number=19;
+// const even=iseven(number);
+
+
+// const Buypen=2;
+// const BuyShirt=3;
+// const BuyTshirth=4;
+// function total(Quantity){
+//     let pen=Buypen*Quantity;
+// }
+// const Quantity=2;
+//  pen=total(Quantity)
+// const totalprice=total(Quantity);
+// console.log(totalprice);
+// const names=["Dipok","abul","kabol","sabol"]
+// console.log(names.length(2));
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let length = fruits.Banana.length;
+// console.log(length);
+
+// i=0;
+// while(i<10){
+//     i++;
+//     console.log(i);
+// }
+// for(i=0;i<=20;i++){
+//     console.log(i);
+// }
+// const food=["Birayani","chicken","polao","mutton"];
+// console.log(food.indexOf("Birayani"));
+
+// function Iseven(number){
+//     if(number%2===0)
+//     console.log("This is even number");
+//     else
+//     console.log("This is odd number");
+// }
+// const number=39;
+// const even=Iseven(number);
+
+// function covert(kilometer){
+//     let meter=kilometer/1000;
+//     return meter;
+// }
+// const kilometer=3000;
+// const done=covert(kilometer);
+// console.log("The meter is=",done);
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
